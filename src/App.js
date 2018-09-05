778c8ed14218c946adb0e3b1815b48d95adfeefa
JoeyAlpha5 <35291886+JoeyAlpha5@users.noreply.github.com>
2018-09-05 18:23:13 +0200

    Upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Banner from './Components/banner';
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Banner/>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
---
+++ src/Components/banner.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+class Banner extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <React.Fragment>
+                <div className="Banner">
+                </div>
+            </React.Fragment>
+         );
+    }
+}
+ 
+export default Banner ;
